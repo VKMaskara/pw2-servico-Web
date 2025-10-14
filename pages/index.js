3ee0d38ad0dc98d15a75fbf30e8adce8b40b92a2
Vitor KAue <rodriguesvitorkaue@gmail.com>
2025-10-13 21:58:23 -0300

    consertando erros

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,30 @@ import {filmes} from "../data/filmes.js"
 
 
 function Home (){
-  console.log(filmes)
-
-  const primeirofilme = filmes[1]
+ 
   const estiloimagem = {width:"300px"}
 
   return<> 
     <h1>FILMES</h1>
     <ul>
-      <li>
+      {
+        filmes.map(( filme ) => {
+          return (
+            <li key={`card-filme-${ filme.id}`}>
 
-        <p>Nome do filme</p>
-
-        <p> {primeirofilme .titulo}</p>
-        <p> {primeirofilme .ano}</p>
-        <p> {primeirofilme .genero}</p>
-        <p> {primeirofilme .nota}</p>
-
-        <img style={estiloimagem} src = {primeirofilme}></img>
-      </li>
+              <p>Nome do filme</p>
+      
+              <p> {filme.titulo}</p>
+              <p> {filme.ano}</p>
+              <p> {filme.genero}</p>
+              <p> {filme.nota}</p>
+      
+              <img style={estiloimagem} src = {filme.poster}></img>
+            </li>
+          );
+        })
+      }
+      
     </ul>
   </>
 
