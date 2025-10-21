7d1e3f00f00374d640c395c5c2740d4314c96202
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2025-10-20 21:58:53 -0300

    semantica

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
 import {filmes} from "../data/filmes.js"
 
+function CardFilmes({filme}){
+   const estiloimagem = {width:"300px"};
+  return(
+    <li>
+        <p>Nome do filme</p>
+
+        <p> {filme.titulo}</p>
+        <p> {filme.ano}</p>
+        <p> {filme.genero}</p>
+        <p> {filme.nota}</p>
+
+        <img style={estiloimagem} src = {filme.poster}></img>
+        
+     </li>
+  );
+}
 
 function Home (){
  
-  const estiloimagem = {width:"300px"}
-
   return<> 
     <h1>FILMES</h1>
     <ul>
       {
         filmes.map(( filme ) => {
           return (
-            <li key={`card-filme-${ filme.id}`}>
-
-              <p>Nome do filme</p>
-      
-              <p> {filme.titulo}</p>
-              <p> {filme.ano}</p>
-              <p> {filme.genero}</p>
-              <p> {filme.nota}</p>
-      
-              <img style={estiloimagem} src = {filme.poster}></img>
-            </li>
+            <CardFilmes  key={`card-filme-${ filme.id}`} filme={filme}/>
           );
         })
       }
